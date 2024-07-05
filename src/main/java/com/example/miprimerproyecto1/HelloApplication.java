1771d315c8594a9a608412c79afa91ad9bf7e3e8
Brendajacome <bcjacomea@uce.edu.ec>
2024-07-05 01:16:36 -0500

    Update HelloApplication.java

--- src/main/java/com/example/miprimerproyecto1/HelloApplication.java
+++ src/main/java/com/example/miprimerproyecto1/HelloApplication.java
@@ -1,22 +1,23 @@
-package org.example.sumativainicial;
+package com.example.miprimerproyecto1;
 
-import  java.util.Scanner;
+import java.util.Scanner;
 
-public class HelloApplication  {
+public class HelloApplication{
     public static void main(String[] args) {
-        for (int i =1 ; i<=100; i++){
-        String salida = "";
-        if (i % 3 ==0){
-            salida += "fizz";
-        }
-        if (i % 5 ==0){
-            salida += "buzz";
-        }
-            if (salida.isEmpty()){
-                salida += i;
-        }
-         System.out.print(salida);
+        int numberA;
+        Scanner read=new Scanner(System.in);
+        System.out.println("Write the first number:");
+        numberA=read.nextInt();
+        int numberB;
+        Scanner read2=new Scanner(System.in);
+        System.out.println("Write the second number:");
+        numberB=read2.nextInt();
+        int addition=0;
+        addition=numberA+numberB;
+            System.out.println("Result addition:"+addition);
+            double division;
+            division= (double) numberA /numberB;
+            System.out.println("Result division:"+division );
+
     }
-  }
 }
-//Jacome Brenda
